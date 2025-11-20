7c0c9314ba41e5d324d4c3537f7f204b5d4221ed
H4T4 <206106605+H4T4@users.noreply.github.com>
2025-11-20 12:21:28 +0000

    live.py agront

---
+++ notebooks/live.py
@@ -0,0 +1,26 @@
+import haiku as hk
+import jax
+import jax.numpy as jnp
+from nucleotide_transformer.pretrained import get_pretrained_model
+
+# Get pretrained model
+parameters, forward_fn, tokenizer, config = get_pretrained_model(
+    model_name="1B_agro_nt",
+    embeddings_layers_to_save=(12,),
+    max_positions=32,
+)
+forward_fn = hk.transform(forward_fn)
+
+# Get data and tokenize it
+sequences = ["ATTCCGATTCCGATTCCG", "ATTTCTCTCTCTCTCTGAGATCGATCGATCGAT"]
+tokens_ids = [b[1] for b in tokenizer.batch_tokenize(sequences)]
+tokens = jnp.asarray(tokens_ids, dtype=jnp.int32)
+
+# Initialize random key
+random_key = jax.random.PRNGKey(0)
+
+# Infer
+outs = forward_fn.apply(parameters, random_key, tokens)
+
+# Get embeddings at layer 20
+print(outs["embeddings_12"].shape)
